3decaab3f886d04e77f3016b88960e24e83cf5da
whaakman <whaakman@intercept.nl>
2020-02-28 19:31:18 +0100

    azure deploy

--- iac/deployment.ps1
+++
@@ -1,6 +0,0 @@
-# Create Resource Group
-$rgName = "rg-demoappisv"
-$location = "westeurope"
-new-azresourcegroup -name $rgName -location $location
-
-New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile .\webapp-sql.json -TemplateParameterFile .\webapp-sql.parameters.json -verbose
