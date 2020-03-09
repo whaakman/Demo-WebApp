ebb093aac7c784f8ac299a92fb6f22fd27896ff8
whaakman <whaakman@intercept.nl>
2020-03-09 17:52:19 +0100

    Only showing failed tests

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -4,4 +4,4 @@ Test-AzTemplate -TemplatePath /temp/
 $TestResults = Test-AzTemplate -TemplatePath /temp/
 $TestFailures =  $TestResults | Where-Object { -not $_.Passed }
 
-Write-Host $TestFailures
+Write-Output $TestFailures
