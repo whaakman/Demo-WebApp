e273bf4831f48e7e455287e5c3f0a18afbf3c527
whaakman <whaakman@intercept.nl>
2020-03-09 20:01:36 +0100

    Added debugging

--- .github/actions/entrypoint.ps1
+++ .github/actions/entrypoint.ps1
@@ -1,7 +1,6 @@
 Import-Module '/arm-ttk/arm-ttk/arm-ttk.psd1'
 
-Write-Host "Github Workspace" $GITHUB_WORKSPACE
-$TemplatePath = '/home/runner/work/Demo-WebApp-ISVs/Demo-WebApp-ISVs/'
+$TemplatePath = '/temp/'
 $TestResults = Test-AzTemplate -TemplatePath $TemplatePath
 $TestFailures =  $TestResults | Where-Object { -not $_.Passed }
 
