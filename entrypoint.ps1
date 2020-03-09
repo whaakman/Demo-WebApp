45b30fc6c37222f0779274b6662e30171fd6b26a
whaakman <whaakman@intercept.nl>
2020-03-09 17:45:42 +0100

    Only showing failed tests

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,2 +1,7 @@
 Import-Module '/arm-ttk/arm-ttk/arm-ttk.psd1'
 Test-AzTemplate -TemplatePath /temp/
+
+$TestResults = Test-AzTemplate -TemplatePath /temp/
+$TestFailures =  $TestResults | Where-Object { -not $_.Passed }
+
+Write-Host $TestFailures
