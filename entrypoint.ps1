aacfc594936876c8fd2e8628e8e5e42fc5aa16dd
whaakman <whaakman@intercept.nl>
2020-03-09 19:14:29 +0100

    Changed path in entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,7 +1,7 @@
 Import-Module '/arm-ttk/arm-ttk/arm-ttk.psd1'
-Test-AzTemplate -TemplatePath /temp/
+# path where checkout clones our repo to
 
-$TestResults = Test-AzTemplate -TemplatePath /temp/
+$TestResults = Test-AzTemplate -TemplatePath /home/runner/work/Demo-WebApp-ISVs/Demo-WebApp-ISVs
 $TestFailures =  $TestResults | Where-Object { -not $_.Passed }
 
 Write-Output $TestFailures
